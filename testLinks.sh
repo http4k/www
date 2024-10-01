2b8708c860b5312e4662de68d7b065f6d64ae371
David Denton <denton.david@gmail.com>
2024-10-01 19:37:21 +0100

    add test links script

--- testLinks.sh
+++ testLinks.sh
@@ -1,3 +1,4 @@
 
 cd src/website/public
-docker run -v $(pwd):/test --rm wjdp/htmltest . -l 3 -s
+docker run -v $(pwd):/test --rm wjdp/htmltest . -l 3 -s | grep -v png | grep -v svg | grep -v js
+cd -
