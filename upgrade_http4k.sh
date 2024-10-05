084a63def7dfc102ee0569b4b935d480bf3f7bb8
David Denton <denton.david@gmail.com>
2024-10-05 16:18:37 +0100

    fixing versions

--- upgrade_http4k.sh
+++ upgrade_http4k.sh
@@ -7,5 +7,5 @@ OLD_VERSION=$(grep -o 'http4k_version=.*' gradle.properties | head -n 1 | cut -d
 
 echo "Old version: $OLD_VERSION"
 
-sed -i "s/\$OLD_VERSION\b/$NEW_VERSION/g" gradle.properties
-sed -i "s/\$OLD_VERSION\b/$NEW_VERSION/g" src/website/config.yaml
+sed -i '' "s/${OLD_VERSION}/${NEW_VERSION}/g" gradle.properties
+sed -i '' "s/${OLD_VERSION}/${NEW_VERSION}/g" src/website/config.yaml
