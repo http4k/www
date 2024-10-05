1b258f219fb5b29ad169bce2987d81f148350f19
David Denton <denton.david@gmail.com>
2024-10-05 16:14:47 +0100

    add scripts

--- upgrade_http4k_connect.sh
+++ upgrade_http4k_connect.sh
@@ -3,7 +3,7 @@ set -e
 
 NEW_VERSION=$1
 
-OLD_VERSION=$(grep -oP 'http4k_connect_version=\K[^ ]+' gradle.properties
+OLD_VERSION=$(grep -o 'http4k_connect_version=.*' gradle.properties | head -n 1 | cut -d '=' -f 2)
 
 echo "Old version: $OLD_VERSION"
 
