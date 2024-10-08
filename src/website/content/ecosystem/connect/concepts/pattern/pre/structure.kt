3aa8eb8f58e7e4a2211af7a24a9546d526c5147a
David Denton <denton.david@gmail.com>
2024-10-08 19:42:19 +0100

    Add import

--- src/website/content/ecosystem/connect/concepts/pattern/pre/structure.kt
+++ src/website/content/ecosystem/connect/concepts/pattern/pre/structure.kt
@@ -3,7 +3,7 @@ package content.ecosystem.connect.concepts.pattern.pre
 import org.http4k.core.HttpHandler
 import org.http4k.core.Method.GET
 import org.http4k.core.Method.POST
-import org.http4k.core.Status.OK
+import org.http4k.core.Status.Companion.OK
 import org.http4k.core.Request
 import org.http4k.core.Response
 import org.http4k.core.then
