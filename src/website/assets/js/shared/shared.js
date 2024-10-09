697e3fe0b0e2e5623a3b94f6bc39b35f8f823a15
Ivan Sanchez <ivan@gourame.com>
2024-10-09 15:35:18 +0100

    Reintroduce search

--- src/website/assets/js/shared/shared.js
+++ src/website/assets/js/shared/shared.js
@@ -2,3 +2,12 @@ import "./_bootstrap";
 
 import "./_header";
 import "./_footer";
+
+// toggle search box via command/ctrl + k
+$(window).keydown(function (event) {
+    if ((event.metaKey || event.ctrlKey) && event.keyCode === 75) {
+        event.preventDefault()
+        $("#search").focus();
+    }
+});
+
