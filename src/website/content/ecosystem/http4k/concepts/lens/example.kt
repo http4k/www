3ab9a3311c2394867f6b0083591ef5af1aea6230
David Denton <denton.david@gmail.com>
2024-10-10 14:23:30 +0100

    fix up code so it's not so long

--- src/website/content/ecosystem/http4k/concepts/lens/example.kt
+++ src/website/content/ecosystem/http4k/concepts/lens/example.kt
@@ -32,14 +32,15 @@ data class CustomType(val value: String)
 
 val requiredCustomQuery = Query.map(::CustomType, { it.value }).required("myCustomType")
 
-//To use the Lens, simply `invoke() or extract()` it using an HTTP message to extract the value, or alternatively `invoke() or inject()` it with the value if we are modifying (via copy) the message:
+//To use the Lens, simply `invoke() or extract()` it using an HTTP message to extract the value, or alternatively
+// `invoke() or inject()` it with the value if we are modifying (via copy) the message:
 val handler: RoutingHttpHandler = routes(
     "/hello/{date:.*}" bind GET to { request: Request ->
         val pathDate: LocalDate = pathLocalDate(request)
         // SAME AS:
         // val pathDate: LocalDate = pathLocalDate.extract(request)
 
-        val customType: CustomType = requiredCustomQuery(request)
+        val customType: public.ecosystem.http4k.concepts.lens.CustomType = requiredCustomQuery(request)
         val anIntHeader: Int? = optionalHeader(request)
 
         val baseResponse = Response(OK)
@@ -51,17 +52,20 @@ val handler: RoutingHttpHandler = routes(
     }
 )
 
-//With the addition of the `CatchLensFailure` filter, no other validation is required when using Lenses, as http4k will handle invalid requests by returning a BAD_REQUEST (400) response.
-val app = ServerFilters.CatchLensFailure.then(handler)(
+//With the addition of the `CatchLensFailure` filter, no other validation is required when using Lenses, as http4k
+// will handle invalid requests by returning a BAD_REQUEST (400) response.
+val app = ServerFilters.CatchLensFailure.then(public.ecosystem.http4k.concepts.lens.handler)(
     Request(
         GET,
         "/hello/2000-01-01?myCustomType=someValue"
     )
 )
 
-//More conveniently for construction of HTTP messages, multiple lenses can be used at once to modify a message, which is useful for properly building both requests and responses in a typesafe way without resorting to string values (especially in URLs which should never be constructed using String concatenation):
+//More conveniently for construction of HTTP messages, multiple lenses can be used at once to modify a message,
+// which is useful for properly building both requests and responses in a typesafe way without resorting to string
+// values (especially in URLs which should never be constructed using String concatenation):
 val modifiedRequest: Request = Request(GET, "http://google.com/{pathLocalDate}").with(
-    pathLocalDate of LocalDate.now(),
-    requiredQuery of "myAmazingString",
-    optionalHeader of 123
+    public.ecosystem.http4k.concepts.lens.pathLocalDate of LocalDate.now(),
+    public.ecosystem.http4k.concepts.lens.requiredQuery of "myAmazingString",
+    public.ecosystem.http4k.concepts.lens.optionalHeader of 123
 )
