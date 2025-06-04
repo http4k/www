bd74cd664e955411eaad561b89eff6a632818405
David Denton <denton.david@gmail.com>
2025-06-04 20:54:12 +0100

    upgrade versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -240,7 +240,7 @@ dependencies {
     testApi("dev.forkhandles:mock4k")
 
     testApi("software.amazon.awssdk:s3:_")
-    testApi("com.azure:azure-search-documents:_")
+//    testApi("com.azure:azure-search-documents:_")
     testApi("com.google.http-client:google-http-client:_")
 
     testApi("io.opentelemetry.contrib:opentelemetry-aws-xray-propagator:_")
