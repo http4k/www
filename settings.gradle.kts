6ce4fc79ff0aa643103bf3c30fbae6119179e3bb
David Denton <denton.david@gmail.com>
2025-09-01 09:13:15 +0100

    Convert to use version catalogues

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,17 +2,4 @@
 
 rootProject.name = "www"
 
-plugins {
-    id("de.fayard.refreshVersions").version("0.60.5")
-}
-
-refreshVersions {
-    rejectVersionIf {
-        candidate.stabilityLevel.isLessStableThan(current.stabilityLevel) ||
-            setOf("milestone", "-RC").map { it.lowercase() }.any { candidate.value.contains(it) } ||
-            Regex("""\d{4}-\d{2}-\d{2}T\d{2}-\d{2}-\d{2}.*""").matches(candidate.value) || // graphql nightlies
-            candidate.value.contains("nf-execution") // graphql nightlies
-    }
-}
-
 gradle.startParameter.isContinueOnFailure = true
